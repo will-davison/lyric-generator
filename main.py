fa3bd61bb4af0569dc9223d5cd2a96b4212272c0
Rowadz <rowadwo@gmail.com>
2022-11-19 17:14:46 +0100

    use the bestChoice function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pronouncing
 import random
+from rhymetest import bestChoice
 
 
 def replace_words_from_song():
@@ -7,33 +8,49 @@ def replace_words_from_song():
         lines = [line for line in f.readlines()]
 
     # with open('oscar-wilde-lord-arthur-savile-s-crime-and-other-stories.txt') as f2:
-        # lines_from_book = [line for line in f2.readlines()]
-        # print(lines_from_book)
+    #     lines_from_book = [line for line in f2.readlines()]
 
     final_list = []
     for line in lines:
         list_of_words = [pronouncing.rhymes(word) for word in line.split(' ')]
         line_as_array = line.split(' ')
         dump_list = []
+        dump_list_2 = []
+        dump_list_3 = []
+        dump_list_4 = []
         # print(line_as_array)
         for i,words in enumerate(list_of_words):
             try:
                 if i % 4 == 0 and len(words):
-                    random_word = random.choice(words)
-                    dump_list.append(random_word)
-                else: 
+                    dump_list.append(random.choice(words))
+                    dump_list_2.append(random.choice(words))
+                    dump_list_3.append(random.choice(words))
+                    dump_list_4.append(random.choice(words))
+                elif i % 3 == 0 and len(words): 
+                    dump_list.append(random.choice(words))
+                    dump_list_2.append(random.choice(words))
+                    dump_list_3.append(random.choice(words))
+                    dump_list_4.append(random.choice(words))
+                else:
                     dump_list.append(line_as_array[i])
+                    dump_list_2.append(line_as_array[i])
+                    dump_list_3.append(line_as_array[i])
+                    dump_list_4.append(line_as_array[i])
             except:
                 pass
-        final_list.append(' '.join(dump_list))
+        
+        if len(line) == 1: continue
+        
+        best_word = bestChoice(
+            line,
+            [' '.join(dump_list), ' '.join(dump_list_2), ' '.join(dump_list_3), ' '.join(dump_list_4)],
+            1,
+            0.1
+        )
+        final_list.append(best_word)
     return final_list
 
 
         
 
-final_results = []
-final_results.append(replace_words_from_song())
-final_results.append(replace_words_from_song())
-final_results.append(replace_words_from_song())
-
-        
+print(' '.join(replace_words_from_song()))
